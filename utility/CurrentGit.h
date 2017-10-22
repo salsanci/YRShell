b8b9d0601a98952d201aedc6ee22ed4a424791ac
Sal Sanci <sal@SalsMacBookPro.local>
2017-10-21 22:40:06 -0700

    Hash commit

--- utility/CurrentGit.h
+++ utility/CurrentGit.h
@@ -1,5 +1,5 @@
 #ifndef CurrrentGit_h
 #define CurrrentGit_h
-#define GIT_COMMIT_HASH "b281e7429f25cc07e9b039a3ddbd1ea8dd5d7456"
-#define GIT_COMMIT_SHORT_HASH "[b281e74]"
+#define GIT_COMMIT_HASH "5c66373e996417dc27674af14c9dfd99d6718d3d"
+#define GIT_COMMIT_SHORT_HASH "[5c66373]"
 #endif
