4e24f2d52469bdcdb42a16566d57c503769d3b40
Sal Sanci <sal@SalsMacBookPro.local>
2017-11-05 10:30:39 -0800

    Makefile fix, hash update

--- utility/CurrentGit.h
+++ utility/CurrentGit.h
@@ -1,5 +1,5 @@
 #ifndef CurrrentGit_h
 #define CurrrentGit_h
-#define GIT_COMMIT_HASH "c9cd6c75559ddb3ae60de70394c3309c339d0dc4"
-#define GIT_COMMIT_SHORT_HASH "[c9cd6c7]"
+#define GIT_COMMIT_HASH "5b223a0f1de646024740d165aa34826151b245cc"
+#define GIT_COMMIT_SHORT_HASH "[5b223a0]"
 #endif
