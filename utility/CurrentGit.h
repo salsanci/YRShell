9732f919fe1a83c3c4a63cc92128a078a26c982a
Sal Sanci <sal@SalsMacBookPro.local>
2018-02-19 20:34:00 -0800

    hash update

--- utility/CurrentGit.h
+++ utility/CurrentGit.h
@@ -1,5 +1,5 @@
 #ifndef CurrrentGit_h
 #define CurrrentGit_h
-#define GIT_COMMIT_HASH "2ea05c1deb9f8200c76ad43d34cf3b1562450a05"
-#define GIT_COMMIT_SHORT_HASH "[2ea05c1]"
+#define GIT_COMMIT_HASH "27752a1951788c59d732624cf689c7f17be1746f"
+#define GIT_COMMIT_SHORT_HASH "[27752a1]"
 #endif
